070576dca3a33c2575192f5dac288f050c2925cc
smallshaq18 <abdulsamad.m1802472@gmail.com>
2025-08-22 20:40:28 -0200

    second trial

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
-// index.js
+var express = require('express');
+var app = express();
+var cors = require('cors');
+
+app.use(cors({optionsSuccessStatus: 200}));
+app.use(express.static('public'));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api/:date?", function(req, res) {
+  let input = req.params.date;
+  let date;
+  
+  if (!input) {
+    date = new Date();
+  } else if (/^\d+$/.test(input)) {
+    date = new Date(parseInt(input));
+  } else {
+    date = new Date(input);
+  }
+  
+  if (isNaN(date.getTime())) {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+  }
+});
+
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+/*// index.js
 // where your node app starts
 
 // init project
@@ -31,7 +72,7 @@ var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-app.get("/api/timestamp/:date?", (req, res) => {
+app.get("/api/:date?", (req, res) => {
   let dateParam = req.params.date;
 
   // If no date param, use current date
@@ -61,4 +102,34 @@ app.get("/api/timestamp/:date?", (req, res) => {
     utc: date.toUTCString()
   });
 });
+
+// Timestamp API
+app.get("/api/:date?", (req, res) => {
+  let dateString = req.params.date;
+
+  let date;
+  // If no date is provided, use current date
+  if (!dateString) {
+    date = new Date();
+  } else {
+    // If input is only numbers, treat it as Unix timestamp
+    if (!isNaN(dateString)) {
+      date = new Date(parseInt(dateString));
+    } else {
+      date = new Date(dateString);
+    }
+  }
+
+  // Handle invalid date
+  if (date.toString() === "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  // Valid date → return unix + utc
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+*/
 
